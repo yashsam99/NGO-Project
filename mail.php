9bd7d4cb6492d7a31a54d1100b5fb2bef1419b9a
yashsam99 <43442214+yashsam99@users.noreply.github.com>
2020-12-07 10:05:04 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,11 @@
+﻿<?php 
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "satyammishra366@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
